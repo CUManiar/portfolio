fe5585689d6874163281c86eb1c3fbac73fd42f2
cumaniar <maniarchirag57@gmail.com>
2019-11-24 23:04:27 +0530

    string length pipe

---
+++ src/app/shared/pipes/string-length.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from "@angular/core";
+
+@Pipe({
+  name: "stringLength"
+})
+export class StringLengthPipe implements PipeTransform {
+  transform(value: any, args: string): any {
+    const limit = args ? parseInt(args, 10) : 10;
+    const trail = "...";
+
+    return value.length > limit ? value.substring(0, limit) + trail : value;
+  }
+}
